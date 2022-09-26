eff0596a31e936dd9d19695184f747cbe8ee2f23
tide525 <t-ide@toki.waseda.jp>
2022-09-26 13:54:48 +0900

    コンバータを更新

--- convert.py
+++ convert.py
@@ -7,14 +7,19 @@ import re
 from typing import Any, Dict, Set
 
 from tqdm import tqdm
+import zenhan
+from pyknp import Juman
 
 
-MIN_STATUS_TEXT_LEN = 8
+MIN_STATUS_TEXT_LEN = 7
+MIN_DIALOGUE_LEN = 3
+
 REPEAT_PATTERN = re.compile(r'(.+?)\1{4}')
 NOT_ASCII_OR_JA_PATTERN = re.compile(r'[^\u0000-\u007f\u3000-\u30ff\u4e00-\u9fff]')
 JA_PATTERN = re.compile(r'[\u3000-\u30ff\u4e00-\u9fff]')
 
-MIN_DIALOGUE_LEN = 6
+
+jumanpp = Juman()
 
 
 def clean_text_or_name(text_or_name: str) -> str:
@@ -29,34 +34,38 @@ def clean_text_or_name(text_or_name: str) -> str:
 def filter_status_text(status_text: str) -> bool:
     if len(status_text) < MIN_STATUS_TEXT_LEN:
         return False
+
     if REPEAT_PATTERN.search(status_text):
         return False
     if NOT_ASCII_OR_JA_PATTERN.search(status_text):
         return False
     if not JA_PATTERN.search(status_text):
         return False
+
+    result = jumanpp.analysis(zenhan.h2z(status_text))
+    if any('未知語' in mrph.imis for mrph in result.mrph_list()):
+        return False
+
     return True
 
 
-def filter_line_dict(
-    line_dict: Dict[str, Any]
-) -> bool:
+def filter_line_dict(line_dict: Dict[str, Any]) -> bool:
     if len(line_dict['dialogue']) < MIN_DIALOGUE_LEN:
         return False
     if len(line_dict['user']) < 2:
         return False
+
     if any(
         line_dict['dialogue'][i]['user_id']
         == line_dict['dialogue'][i + 1]['user_id']
         for i in range(len(line_dict['dialogue']) - 1)
     ):
         return False
+
     return True
 
 
-def convert_dialogues_into_lines(
-    dialogues: Dict[str, Any]
-) -> Set[str]:
+def convert_dialogues_into_lines(dialogues: Dict[str, Any]) -> Set[str]:
     lines = set()
 
     for dialogue in dialogues:
@@ -118,20 +127,18 @@ def main():
     )
     args = parser.parse_args()
 
-    lines = set()
+    with open(args.output_jsonl, 'w', encoding='utf-8') as f_output:
+        data_jsons = glob.glob(os.path.join(args.data_dir, '**', '*.json'), recursive=True)
+        for data_json in tqdm(data_jsons):
+            with open(data_json, encoding='utf-8') as f_data:
+                try:
+                    dialogues = json.load(f_data)
+                    lines = convert_dialogues_into_lines(dialogues)
+                    for line in lines:
+                        f_output.write(line + '\n')
 
-    json_files = glob.glob(os.path.join(args.data_dir, '**', '*.json'), recursive=True)
-    for json_file in tqdm(json_files):
-        with open(json_file, encoding='utf-8') as f:
-            try:
-                dialogues = json.load(f)
-                lines |= convert_dialogues_into_lines(dialogues)
-            except json.JSONDecodeError as err:
-                print(err)
-
-    with open(args.output_jsonl, 'w', encoding='utf-8') as f:
-        for line in lines:
-            f.write(line + '\n')
+                except json.JSONDecodeError as err:
+                    print(err)
 
 
 if __name__ == '__main__':
